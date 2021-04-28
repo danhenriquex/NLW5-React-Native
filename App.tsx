986cca5b56c3d8a0803e3a94b494422c12f5a4b6
danilohenrique <danilosantana@mat.ci.ufpb.br>
2021-04-28 18:20:26 -0300

    Day 05 - Notifications

--- App.tsx
+++ App.tsx
@@ -1,18 +1,41 @@
-import React from "react";
-import { View, Text } from "react-native";
-import AppLoading from "expo-app-loading";
-import Routes from "./src/routes";
 import {
-  useFonts,
   Jost_400Regular,
   Jost_600SemiBold,
+  useFonts,
 } from "@expo-google-fonts/jost";
+import AppLoading from "expo-app-loading";
+import * as Notifications from "expo-notifications";
+import React, { useEffect } from "react";
+import { PlantProps } from "./src/libs/storage";
+import Routes from "./src/routes";
 
 export default function App() {
   const [fonstLoaded] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold,
   });
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      async (notification) => {
+        const data = notification.request.content.data.plant as PlantProps;
+        console.log(data);
+      }
+    );
+
+    return () => subscription.remove();
+    
+    // ! Don't uncomment
+    // async function notifications() { 
+    //   await Notifications.cancelAllScheduledNotificationsAsync();
+
+    //   const data = await Notifications.getAllScheduledNotificationsAsync();
+    //   console.log("## NOTIFICAÇÕES AGENDADAS ##");
+    //   console.log(data);
+    // }
+
+    // notifications();
+  }, []);
 
   if (!fonstLoaded) {
     return <AppLoading />;
